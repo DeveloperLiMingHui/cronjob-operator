8508be85c2ae07da41c812e8a1d60cd715d435aa
gary <gary@canopywave.com>
2026-02-27 15:13:14 +0800

    add: cr(config/samples/batch_v1_cronjob.yaml), run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export ENABLE_WEBHOOKS=false
+make run
+
+
+# kubectl create -f config/samples/batch_v1_cronjob.yaml
+
+# kubectl get cronjob.batch.tutorial.kubebuilder.io -o yaml
+# kubectl get job
+
+
+
