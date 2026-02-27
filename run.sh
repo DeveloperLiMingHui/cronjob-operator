32ab8edbd85263f8ec79fc9b88b5abc87816bc1b
gary <gary@canopywave.com>
2026-02-27 16:01:57 +0800

    fix: rbac make manifests(job)

--- run.sh
+++ run.sh
@@ -15,3 +15,14 @@ make run
 
 # Fri Feb 27 07:27:02 UTC 2026
 # Hello from the Kubernetes cluster
+
+
+# make docker-build docker-push IMG=xlmh/cronjob-operator:v0.0.1
+
+# kind load docker-image xlmh/cronjob-operator:v0.0.1
+
+# make install
+
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.4/cert-manager.yaml
+
+make deploy IMG=xlmh/cronjob-operator:v0.0.1
