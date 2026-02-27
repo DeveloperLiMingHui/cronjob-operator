5ac8ea505fc9d6c5c5da300ffa667dffefe3f034
gary <gary@canopywave.com>
2026-02-27 14:46:05 +0800

    add: cronjob_webhook

--- internal/webhook/v1/cronjob_webhook.go
+++ internal/webhook/v1/cronjob_webhook.go
@@ -19,6 +19,11 @@ package v1
 import (
 	"context"
 
+	"github.com/robfig/cron"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	validationutils "k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -34,7 +39,12 @@ var cronjoblog = logf.Log.WithName("cronjob-resource")
 func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr, &batchv1.CronJob{}).
 		WithValidator(&CronJobCustomValidator{}).
-		WithDefaulter(&CronJobCustomDefaulter{}).
+		WithDefaulter(&CronJobCustomDefaulter{
+			DefaultConcurrencyPolicy:          batchv1.AllowConcurrent,
+			DefaultSuspend:                    false,
+			DefaultSuccessfulJobsHistoryLimit: 3,
+			DefaultFailedJobsHistoryLimit:     1,
+		}).
 		Complete()
 }
 
@@ -48,16 +58,43 @@ func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
 type CronJobCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
+	// Default values for various CronJob fields
+	DefaultConcurrencyPolicy          batchv1.ConcurrencyPolicy
+	DefaultSuspend                    bool
+	DefaultSuccessfulJobsHistoryLimit int32
+	DefaultFailedJobsHistoryLimit     int32
 }
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind CronJob.
 func (d *CronJobCustomDefaulter) Default(_ context.Context, obj *batchv1.CronJob) error {
 	cronjoblog.Info("Defaulting for CronJob", "name", obj.GetName())
-
-	// TODO(user): fill in your defaulting logic.
-
+	// Set default values
+	d.applyDefaults(obj)
 	return nil
+}
+
+// applyDefaults applies default values to CronJob fields.
+func (d *CronJobCustomDefaulter) applyDefaults(cronJob *batchv1.CronJob) {
+
+	if cronJob.Spec.ConcurrencyPolicy == "" {
+		cronJob.Spec.ConcurrencyPolicy = d.DefaultConcurrencyPolicy
+	}
+
+	if cronJob.Spec.Suspend == nil {
+		cronJob.Spec.Suspend = new(bool)
+		*cronJob.Spec.Suspend = d.DefaultSuspend
+	}
+
+	if cronJob.Spec.SuccessfulJobsHistoryLimit == nil {
+		cronJob.Spec.SuccessfulJobsHistoryLimit = new(int32)
+		*cronJob.Spec.SuccessfulJobsHistoryLimit = d.DefaultSuccessfulJobsHistoryLimit
+	}
+
+	if cronJob.Spec.FailedJobsHistoryLimit == nil {
+		cronJob.Spec.FailedJobsHistoryLimit = new(int32)
+		*cronJob.Spec.FailedJobsHistoryLimit = d.DefaultFailedJobsHistoryLimit
+	}
+
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -79,16 +116,16 @@ func (v *CronJobCustomValidator) ValidateCreate(_ context.Context, obj *batchv1.
 
 	// TODO(user): fill in your validation logic upon object creation.
 
-	return nil, nil
+	return nil, validateCronJob(obj)
+
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
 func (v *CronJobCustomValidator) ValidateUpdate(_ context.Context, oldObj, newObj *batchv1.CronJob) (admission.Warnings, error) {
 	cronjoblog.Info("Validation for CronJob upon update", "name", newObj.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
+	return nil, validateCronJob(newObj)
 
-	return nil, nil
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
@@ -98,4 +135,50 @@ func (v *CronJobCustomValidator) ValidateDelete(_ context.Context, obj *batchv1.
 	// TODO(user): fill in your validation logic upon object deletion.
 
 	return nil, nil
+}
+
+func validateScheduleFormat(schedule string, fldPath *field.Path) *field.Error {
+	if _, err := cron.ParseStandard(schedule); err != nil {
+		return field.Invalid(fldPath, schedule, err.Error())
+	}
+	return nil
+}
+
+func validateCronJobSpec(cronjob *batchv1.CronJob) *field.Error {
+	// The field helpers from the kubernetes API machinery help us return nicely
+	// structured validation errors.
+	return validateScheduleFormat(
+		cronjob.Spec.Schedule,
+		field.NewPath("spec").Child("schedule"))
+}
+
+func validateCronJobName(cronjob *batchv1.CronJob) *field.Error {
+	if len(cronjob.Name) > validationutils.DNS1035LabelMaxLength-11 {
+		// The job name length is 63 characters like all Kubernetes objects
+		// (which must fit in a DNS subdomain). The cronjob controller appends
+		// a 11-character suffix to the cronjob (`-$TIMESTAMP`) when creating
+		// a job. The job name length limit is 63 characters. Therefore cronjob
+		// names must have length <= 63-11=52. If we don't validate this here,
+		// then job creation will fail later.
+		return field.Invalid(field.NewPath("metadata").Child("name"), cronjob.Name, "must be no more than 52 characters")
+	}
+	return nil
+}
+
+// validateCronJob validates the fields of a CronJob object.
+func validateCronJob(cronjob *batchv1.CronJob) error {
+	var allErrs field.ErrorList
+	if err := validateCronJobName(cronjob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if err := validateCronJobSpec(cronjob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "batch.tutorial.kubebuilder.io", Kind: "CronJob"},
+		cronjob.Name, allErrs)
 }
