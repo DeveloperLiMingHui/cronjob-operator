91900bff0e6b6cdc1612b200ba3494e98d82bc76
gary <gary@canopywave.com>
2026-03-02 11:19:44 +0800

    update: Implementing conversion

--- api/v2/cronjob_conversion.go
+++ api/v2/cronjob_conversion.go
@@ -17,7 +17,9 @@ limitations under the License.
 package v2
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
@@ -30,12 +32,38 @@ func (src *CronJob) ConvertTo(dstRaw conversion.Hub) error {
 	log.Printf("ConvertTo: Converting CronJob from Spoke version v2 to Hub version v1;"+
 		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name)
 
-	// TODO(user): Implement conversion logic from v2 to v1
-	// Example: Copying Spec fields
-	// dst.Spec.Size = src.Spec.Replicas
-
-	// Copy ObjectMeta to preserve name, namespace, labels, etc.
+	sched := src.Spec.Schedule
+	scheduleParts := []string{"*", "*", "*", "*", "*"}
+	if sched.Minute != nil {
+		scheduleParts[0] = string(*sched.Minute)
+	}
+	if sched.Hour != nil {
+		scheduleParts[1] = string(*sched.Hour)
+	}
+	if sched.DayOfMonth != nil {
+		scheduleParts[2] = string(*sched.DayOfMonth)
+	}
+	if sched.Month != nil {
+		scheduleParts[3] = string(*sched.Month)
+	}
+	if sched.DayOfWeek != nil {
+		scheduleParts[4] = string(*sched.DayOfWeek)
+	}
+	dst.Spec.Schedule = strings.Join(scheduleParts, " ")
+	// ObjectMeta
 	dst.ObjectMeta = src.ObjectMeta
+
+	// Spec
+	dst.Spec.StartingDeadlineSeconds = src.Spec.StartingDeadlineSeconds
+	dst.Spec.ConcurrencyPolicy = batchv1.ConcurrencyPolicy(src.Spec.ConcurrencyPolicy)
+	dst.Spec.Suspend = src.Spec.Suspend
+	dst.Spec.JobTemplate = src.Spec.JobTemplate
+	dst.Spec.SuccessfulJobsHistoryLimit = src.Spec.SuccessfulJobsHistoryLimit
+	dst.Spec.FailedJobsHistoryLimit = src.Spec.FailedJobsHistoryLimit
+
+	// Status
+	dst.Status.Active = src.Status.Active
+	dst.Status.LastScheduleTime = src.Status.LastScheduleTime
 
 	return nil
 }
@@ -46,12 +74,37 @@ func (dst *CronJob) ConvertFrom(srcRaw conversion.Hub) error {
 	log.Printf("ConvertFrom: Converting CronJob from Hub version v1 to Spoke version v2;"+
 		"source: %s/%s, target: %s/%s", src.Namespace, src.Name, dst.Namespace, dst.Name)
 
-	// TODO(user): Implement conversion logic from v1 to v2
-	// Example: Copying Spec fields
-	// dst.Spec.Replicas = src.Spec.Size
+	schedParts := strings.Split(src.Spec.Schedule, " ")
+	if len(schedParts) != 5 {
+		return fmt.Errorf("invalid schedule: not a standard 5-field schedule")
+	}
+	partIfNeeded := func(raw string) *CronField {
+		if raw == "*" {
+			return nil
+		}
+		part := CronField(raw)
+		return &part
+	}
+	dst.Spec.Schedule.Minute = partIfNeeded(schedParts[0])
+	dst.Spec.Schedule.Hour = partIfNeeded(schedParts[1])
+	dst.Spec.Schedule.DayOfMonth = partIfNeeded(schedParts[2])
+	dst.Spec.Schedule.Month = partIfNeeded(schedParts[3])
+	dst.Spec.Schedule.DayOfWeek = partIfNeeded(schedParts[4])
 
-	// Copy ObjectMeta to preserve name, namespace, labels, etc.
+	// ObjectMeta
 	dst.ObjectMeta = src.ObjectMeta
+
+	// Spec
+	dst.Spec.StartingDeadlineSeconds = src.Spec.StartingDeadlineSeconds
+	dst.Spec.ConcurrencyPolicy = ConcurrencyPolicy(src.Spec.ConcurrencyPolicy)
+	dst.Spec.Suspend = src.Spec.Suspend
+	dst.Spec.JobTemplate = src.Spec.JobTemplate
+	dst.Spec.SuccessfulJobsHistoryLimit = src.Spec.SuccessfulJobsHistoryLimit
+	dst.Spec.FailedJobsHistoryLimit = src.Spec.FailedJobsHistoryLimit
+
+	// Status
+	dst.Status.Active = src.Status.Active
+	dst.Status.LastScheduleTime = src.Status.LastScheduleTime
 
 	return nil
 }
